0982091470b1d7798ba438c41b5d37cdc797330f
Shrey <shrey.chowdhary1@gmail.com>
2016-10-16 18:34:18 -0400

    h

--- net.py
+++ net.py
@@ -38,10 +38,10 @@ class Net(object):
         self.yhat = self.forward(X)
         self.delta3 = numpy.multiply(-(y-self.yhat), self.sigmoidPrime(self.z3))
         dW2 = numpy.dot(self.a2.T,self.delta3)
-        dB2 = self.delta3
+        dB2 = numpy.su(self.delta3, axis = 0, keepdims = true)
         self.delta2 = numpy.multiply(numpy.dot(self.delta3,self.W2.T) , self.sigmoidPrime(self.z2))
         dW1 = numpy.dot(X.T,self.delta2)
-        dB1 = self.delta2
+        dB1 = numpy.sum(self.delta2,axis = 0)
         return dW1, dW2, dB1, dB2
 
     def costFunction(self, X, y):
