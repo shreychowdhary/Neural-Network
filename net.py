c79a9ac4e4716c8b378af4ec834ee0506b2bbbd0
Shrey <shrey.chowdhary1@gmail.com>
2016-10-15 22:19:18 -0400

    working mini batch

--- net.py
+++ net.py
@@ -8,6 +8,13 @@ class Net(object):
         self.W1 = numpy.random.randn(self.inputLayerSize,self.hiddenLayerSize)
         self.W2 = numpy.random.randn(self.hiddenLayerSize,self.outputLayerSize)
 
+    def training(X, runs, learningRate, sampleSize):
+        #for i in xrange(runs):
+        print X
+        rIndex = np.random.randint(0,X.shape[0],sampleSize)
+        sample = X[rIndex]
+        print sample
+
     def forward(self,X):
         self.z2 = numpy.mat(X) * numpy.mat(self.W1)
         self.a2 = self.sigmoid(self.z2)
@@ -15,18 +22,18 @@ class Net(object):
         a3 = self.sigmoid(self.z3)
         return a3
 
-    def costFunction(self, X, y):
-        #Compute cost for given X,y, use weights already stored in class.
-        self.yHat = self.forward(X)
-        J = 0.5*sum(numpy.square(y-self.yHat))
-        return J
-
     def backprop(self,X,y):
         self.yhat = self.forward(X)
         delta = numpy.multiply(-(y-self.yhat), self.sigmoidPrime(self.z3))
         dW2 = numpy.mat(self.a2.T) * numpy.mat(delta)
         dW1 = numpy.mat(X.T) * ((numpy.mat(delta) * numpy.mat(self.W2.T)) * self.sigmoidPrime(self.z2))
         return dW1, dW2
+
+    def costFunction(self, X, y):
+        #Compute cost for given X,y, use weights already stored in class.
+        self.yHat = self.forward(X)
+        J = 0.5*sum(numpy.square(y-self.yHat))
+        return J
 
     def sigmoid(self,z):
         return 1/(1+numpy.exp(-z))
