a8995e2738977cfd2d74ae7fbc1f00cc493252a6
Shrey <shrey.chowdhary1@gmail.com>
2016-10-15 20:30:35 -0400

    fixed bugs

--- net.py
+++ net.py
@@ -12,7 +12,7 @@ class Net(object):
         self.z2 = numpy.mat(X) * numpy.mat(self.W1)
         self.a2 = self.sigmoid(self.z2)
         self.z3 = numpy.mat(self.a2) * numpy.mat(self.W2)
-        self.a3 = self.sigmoid(self.z3)
+        a3 = self.sigmoid(self.z3)
         return a3
 
     def sigmoid(self,z):
