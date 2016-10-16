e8ffb9f284d89c2c7ba49fb9276d5de219b51cc3
Shrey <shrey.chowdhary1@gmail.com>
2016-10-15 20:03:24 -0400

    fixed bug

--- net.py
+++ net.py
@@ -11,7 +11,7 @@ class Net(object):
     def forward(self,X):
         self.z2 = numpy.mat(self.W1) * numpy.mat(X)
         self.a2 = self.sigmoid(self.z2)
-        self.z3 = numpy.mat(self.W2) * numpy.mat(a2)
+        self.z3 = numpy.mat(self.W2) * numpy.mat(self.a2)
         self.a3 = self.sigmoid(self.z3)
         return a3
 
