63edf7fa3476daed9040f4a80f943c06eb4809d7
Shrey <shrey.chowdhary1@gmail.com>
2016-10-17 00:04:57 -0400

    f

--- net.py
+++ net.py
@@ -19,7 +19,6 @@ class Net(object):
                 dW1,dW2 = self.backprop(X[j:j+sampleSize],y[j:j+sampleSize])
                 self.W1 -= learningRate * dW1
                 self.W2 -= learningRate * dW2
-                learningRate -= .01
             print self.costFunction(X,y)
 
 
